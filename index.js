1fe61510ffed1937d4a7fe94eff630f8f8854e9b
Mathew <mathewrios1996@gmail.com>
2022-01-10 19:01:43 -0500

    event listeners

--- index.js
+++ index.js
@@ -1,79 +1,244 @@
-//truncate these strings to 250 characters and add ellipses if they are longer than that
-/*const descriptions = document.querySelectorAll(".description-display");
-for (let desc of descriptions.values()) {
-    let content = desc.innerText;
-  
-    if (content.length > 250) {
-      content = content.slice(0, 250);
-      content = content + "...";
+console.log(document);
+
+const heading = document.querySelector("h1");
+console.log(heading);
+
+const value = document.querySelector(".value");
+console.log(value);
+
+const button = document.querySelector("button");
+console.log(button);
+
+const area = document.querySelector(".area-display");
+console.log(area);
+
+const statDiv = document.querySelector(".stat div");
+console.log(statDiv);
+
+const hello = document.querySelector(".hello");
+console.log(hello);
+
+// Find all the buttons on the page
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+
+// Get a list of all `<h3>` elements
+const heading3List = document.querySelectorAll("h3");
+
+// Iterate over the list and print each one
+for (let element of heading3List.values()) {
+  console.log(element);
+}
+
+// Get a list of all `<h2>` elements
+const heading2List = document.querySelectorAll("h2");
+
+// Iterate over the list and print each one
+for (let i = 0; i < heading2List.length; i++) {
+    const element = heading2List[i];
+    console.log(element);
+  }
+
+//Get a list of all the <div> elements containing ratings on the page.
+//Log them to the console using the values() method.
+const ratingList = document.querySelectorAll("div .rating-display");
+for (let rating of ratingList.values()) {
+    console.log(rating);
+}
+
+//Get a list of all the <div> elements containing areas on the page. 
+//Log them to the console using a simple for loop
+const areaList = document.querySelectorAll("div .area-display");
+for (let i = 0; i < areaList.length; i++) {
+    const area = areaList[i];
+    console.log(area);
+}
+
+//Add event listener to a button
+//select a button on the page
+const firstBtn = document.querySelector("button");
+//call the addEventListener() method on this button
+firstBtn.addEventListener("click", (event) => {
+  console.log("You clicked the button", event);
+});
+
+//you can get the specific element that fired the event with the target property of the event object
+firstBtn.addEventListener("click", (event) => {
+  console.log(event.target);
+});
+
+// Select all the buttons for all the parks
+const allBtns = document.querySelectorAll(".rate-button");
+
+// Iterate through the list of buttons and add an event handler to each
+allBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    console.log(event.target.parentNode);
+  });
+});
+
+//the following code changes the background color
+allBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const park = event.target.parentNode;
+    park.style.backgroundColor = "#c8e6c9";
+  });
+});
+
+// Select the `nameSorter` link
+//const nameSorter = document.querySelector("#name-sorter");
+
+/*
+// Add an event listener
+nameSorter.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("You clicked the name sorter");
+});
+*/
+
+/*
+1. Get the <main> element that contains all the parks.
+
+2. Get a NodeList of all the parks.
+
+3. Empty the <main> element.
+
+4. Convert the NodeList to an array for convenience of sorting.
+
+5. Sort the array using techniques that you learned previously.
+
+6. Iterate through the sorted array and append each park to <main>.
+
+// Select the `nameSorter` link
+const nameSorter = document.querySelector("#name-sorter");
+
+// Add an event listener
+nameSorter.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  // 1. Get the main element
+  const main = document.querySelector("main");
+
+  // 2. Get the list of parks
+  const parksList = main.querySelectorAll(".park-display");
+
+  // 3. Empty the main element
+  main.innerHTML = "";
+
+  // 4. Create an array
+  const parksArray = Array.from(parksList);
+
+  // 5. Sort the array
+  parksArray.sort((parkA, parkB) => {
+    const parkAName = parkA.querySelector("h2").innerText;
+    const parkBName = parkB.querySelector("h2").innerText;
+    if (parkAName < parkBName) {
+      return -1;
+    } else if (parkAName > parkBName) {
+      return 1;
+    } else {
+      return 0;
     }
-  
-    desc.innerText = content;
-    console.log(content);
+  });
+
+  // 6. Insert each park into the DOM
+  parksArray.forEach((park) => {
+    main.appendChild(park);
+  });
+});
+*/
+
+//The following code is equivalent to the above
+
+// function for sorting by name
+const sortByName = (parkA, parkB) => {
+  const parkAName = parkA.querySelector("h2").innerText;
+  const parkBName = parkB.querySelector("h2").innerText;
+  if (parkAName < parkBName) {
+    return -1;
+  } else if (parkAName > parkBName) {
+    return 1;
+  } else {
+    return 0;
   }
-  //once the page loads, you'll see that the description of each park is less than or equal to 250 characters, plus the ellipses
- */
-  const descriptions = document.querySelectorAll(".description-display");
-  for (let desc of descriptions.values()) {
-    let content = desc.innerText;
-  
-    if (content.length > 250) {
-      content = content.slice(0, 250);
-      content = content + '<a href="#">...</a>';
-    }
-  
-    desc.innerHTML = content;
-  }
+};
 
-  /*
-  //bold any rating value that is greater than 4.7. You would have to select all rating values
-  const ratings = document.querySelectorAll(".rating-display .value");
-  // iterate through the list and get each actual rating value using the innerText property.
-  //This returns a string value, but you want to work with the rating as a number. So, use the parseFloat() method to convert a string into a floating point number
-  for (let rating of ratings) {
-    let ratingValue = parseFloat(rating.innerText);
-    //check the condition. If ratingValue > 4.7, then set the fontWeight style to bold
-    if (ratingValue > 4.7) {
-        rating.style.fontWeight = "bold";
-        rating.style.color = "#3ba17c";
-      }
-  }
-  */
+// function for sorting by rating
+const sortByRating = (parkA, parkB) => {
+  const parkARating = parseFloat(
+    parkA.querySelector(".rating-display > .value").innerText
+  );
+  const parkBRating = parseFloat(
+    parkB.querySelector(".rating-display > .value").innerText
+  );
+  return parkBRating - parkARating;
+};
 
-  //When the rating is greater than 4.7, you want to replace the value class with the high-rating class
-  const ratings = document.querySelectorAll(".rating-display .value");
-  for (let rating of ratings) {
-    let ratingValue = parseFloat(rating.innerText);
-  
-    if (ratingValue > 4.7) {
-      rating.classList.add("high-rating");
-      rating.classList.remove("value");
-    }
-  }
+// function for handling the nameSorter click
+const nameSorterClickHandler = (event) => {
+  event.preventDefault();
 
-  //Create a new element with text
-  //First, select the parks on the page using the park class as a selector
-  const parks = document.querySelectorAll(".park-display");
-  //Then, get the number of parks using the length property of the list:
-  const numberParks = parks.length;
-  //Next, create a new element. In this case, you should use a <div>
-  const newElement = document.createElement("div");
-  //You have access to all the properties and methods that were used above to modify an element. 
-  //You can set the text of this element with the innerText property, like this:
-  newElement.innerText = `${numberParks} exciting parks to visit`;
-  newElement.classList.add("header-statement");
-  //Finally, to add the new element to the DOM, select the <header> element and use the appendChild() method to add the new element to the <header>
-  const header = document.querySelector("header");
-  header.appendChild(newElement);
+  // 1.  get the main element
+  const main = document.querySelector("main");
+
+  // 2. get the list of parks
+  const parksList = main.querySelectorAll(".park-display");
+
+  // 3. empty the main
+  main.innerHTML = "";
+
+  // 4. create an array
+  const parksArray = Array.from(parksList);
+
+  // 5. sort the array
+  parksArray.sort(sortByName);
+
+  // 6. Insert each park into the DOM
+  parksArray.forEach((park) => {
+    main.appendChild(park);
+  });
+};
+
+// function to handle the ratingSorter click
+const ratingSorterClickHandler = (event) => {
+  event.preventDefault();
+
+  // 1.  get the main element
+  const main = document.querySelector("main");
+
+  // 2. get the list of parks
+  const parksList = main.querySelectorAll(".park-display");
+
+  // 3. empty the main
+  main.innerHTML = "";
+
+  // 4. create an array
+  const parksArray = Array.from(parksList);
+
+  // 5. sort the array
+  parksArray.sort(sortByRating);
+
+  // 6. Insert each park into the DOM
+  parksArray.forEach((park) => {
+    main.appendChild(park);
+  });
+};
+
+// The code that runs once the DOM is loaded
+const main = () => {
+  // select the nameSorter link
+  const nameSorter = document.querySelector("#name-sorter");
+
+  // add an event listener
+  nameSorter.addEventListener("click", nameSorterClickHandler);
+
+  // select the ratingSorter link
+  const ratingSorter = document.querySelector("#rating-sorter");
+
+  // add an event listener
+  ratingSorter.addEventListener("click", ratingSorterClickHandler);
+};
 
 
-  //Removing DOM elements
-  
-  // Get the parent element of all parks
-const main = document.querySelector("main");
-
-// Select a single park
-const park = main.querySelector(".park-display");
-
-// Remove that park
-main.removeChild(park);
+  // Add event listener for DOMContentLoaded
+window.addEventListener("DOMContentLoaded", main);
